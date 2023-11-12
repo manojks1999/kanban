b1f339c0a325139ba7201c2815feed174a5c0ab5
manojks1999 <64463831+manojks1999@users.noreply.github.com>
2023-11-12 19:33:51 +0530

    Update index.js

--- src/v1/routes/index.js
+++ src/v1/routes/index.js
@@ -1,12 +1,11 @@
 var router = require('express').Router()
 
+router.use('/boards', require('./board'))
+router.use('/boards/:boardId/sections', require('./section'))
+router.use('/boards/:boardId/tasks', require('./task'))
 router.use('/test', (req, res) => {
     res.status(201).json({
         'message': "Successfully running"
     })
 })
-router.use('/boards', require('./board'))
-router.use('/boards/:boardId/sections', require('./section'))
-router.use('/boards/:boardId/tasks', require('./task'))
-
 module.exports = router;
